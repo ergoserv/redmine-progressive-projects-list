5b67b1b417bf6f33fecee84784b1afb317d5a79c
Dmitry Babenko <dmitry@babenko.in.ua>
2013-12-18 12:56:32 +0200

    Drop ChiliProject support

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Progressive Projects List
 =========================
 
-Projects list with blackjack and other features for [Redmine](http://www.redmine.org/) and [ChiliProject](https://www.chiliproject.org/).
+Projects list with blackjack and other features for [Redmine](http://www.redmine.org/).
 
 ![Progressive Projects List Screenshot](http://stgeneral.github.io/redmine-progressive-projects-list/images/screenshots/v020/progressive-projects-list-v020-progress.png)
 
@@ -13,7 +13,8 @@ Installation notes
 Plugin's directory should be named progressive_projects_list exactly.
 Other installation instructions are generic and can be found at Redmine Plugins page - http://www.redmine.org/projects/redmine/wiki/Plugins
 
-Compatible with Redmine 2.4, 2.3, 2.2, 2.1 and ChiliProject 3.4.
+Compatible with Redmine 2.4, 2.3, 2.2, 2.1.
+For ChiliProject 3.4 use [version 0.5.1](https://github.com/stgeneral/redmine-progressive-projects-list/releases/tag/v0.5.1).
 
 Other Proressive plugins for Redmine
 ------------------------------------
--- init.rb
+++ init.rb
@@ -9,6 +9,7 @@ Redmine::Plugin.register :progressive_projects_list do
   version '0.5.1'
   url 'http://stgeneral.github.io/redmine-progressive-projects-list/'
   author_url 'https://github.com/stgeneral'
+  requires_redmine :version_or_higher => '2.1.0'
 
   settings :default => {
     'show_project_description'  => false,
@@ -19,17 +20,7 @@ Redmine::Plugin.register :progressive_projects_list do
   }, :partial => 'settings/progressive_projects_list'
 end
 
-if Rails::VERSION::MAJOR >= 3
-  require 'progressive_projects_list'
-  require 'progressive/projects_helper_patch'
-  require 'progressive/projects_list_view_listener'
-  require 'progressive/recent_projects_view_listener'
-else
-  # Rails 2.x (ChiliProject) compatibility
-  require 'dispatcher'
-  Dispatcher.to_prepare :progressive_projects_list do
-    require_dependency 'progressive_projects_list'
-    require_dependency 'progressive/projects_helper_patch'
-    require_dependency 'progressive/application_helper_patch'
-  end
-end
+require 'progressive_projects_list'
+require 'progressive/projects_helper_patch'
+require 'progressive/projects_list_view_listener'
+require 'progressive/recent_projects_view_listener'
