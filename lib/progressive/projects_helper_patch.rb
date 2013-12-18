5a2b88ad881291d46eede3a9cf95c45c8436bdbf
Dmitry Babenko <dmitry@babenko.in.ua>
2013-12-18 12:45:06 +0200

    Fix indentation

--- lib/progressive/projects_helper_patch.rb
+++ lib/progressive/projects_helper_patch.rb
@@ -2,79 +2,79 @@ module Progressive::ProjectsHelperPatch
   def self.included(base) # :nodoc:
     base.class_eval do
 
-    def progressive_setting(key)
-      if request.params[:progressive]
-        session[:progressive] = true
-        session[key] = request.params[key]
-      elsif session[:progressive]
-        session[key]
-      else
-        Setting.plugin_progressive_projects_list[key.to_s]
-      end
-    end
-
-    def progressive_setting?(key)
-      progressive_setting(key).present?
-    end
-
-    def render_project_hierarchy_with_progress_bars(projects)
-      render_project_nested_lists(projects) do |project|
-        s = link_to_project(project, {}, :class => "#{project.css_classes} #{User.current.member_of?(project) ? 'my-project' : nil}")
-        if !progressive_setting?(:show_only_for_my_projects) || User.current.member_of?(project)
-          if progressive_setting?(:show_project_menu)
-            s << render_project_menu(project) + '<br />'.html_safe
-          end
-          if project.description.present? && progressive_setting?(:show_project_description)
-            s << content_tag('div', textilizable(project.short_description, :project => project), :class => 'wiki description')
-          end
-          if progressive_setting?(:show_project_progress) && User.current.allowed_to?(:view_issues, project)
-            s << render_project_progress_bars(project)
-          end
+      def progressive_setting(key)
+        if request.params[:progressive]
+          session[:progressive] = true
+          session[key] = request.params[key]
+        elsif session[:progressive]
+          session[key]
+        else
+          Setting.plugin_progressive_projects_list[key.to_s]
         end
-        s
-      end
-    end
-
-    # Returns project's and its versions' progress bars
-    def render_project_progress_bars(project)
-      project.extend(Progressive::ProjectDecorator)
-      s = ''
-      if project.issues.open.any?
-        s << "<div>" + l(:label_issue_plural) + ": " +
-          link_to(l(:label_x_open_issues_abbr, :count => project.issues.open.count), :controller => 'issues', :action => 'index', :project_id => project, :set_filter => 1) +
-          " <small>(" + l(:label_total) + ": #{project.issues.count})</small> "
-        s << due_date_tag(project.opened_due_date) if project.opened_due_date
-        s << "</div>"
-        s << progress_bar([project.issues_closed_percent, project.issues_completed_percent], :width => '30em', :legend => '%0.0f%' % project.issues_closed_percent)
       end
 
-      if project.versions.open.any?
-        s << "<div>"
-        project.versions.open.reverse_each do |version|
-          next if version.completed?
-          s << l(:label_version) + " " + link_to_version(version) + ": " +
-            link_to(l(:label_x_open_issues_abbr, :count => version.open_issues_count), :controller => 'issues', :action => 'index', :project_id => version.project, :status_id => 'o', :fixed_version_id => version, :set_filter => 1) +
-            "<small> / " + link_to_if(version.closed_issues_count > 0, l(:label_x_closed_issues_abbr, :count => version.closed_issues_count), :controller => 'issues', :action => 'index', :project_id => version.project, :status_id => 'c', :fixed_version_id => version, :set_filter => 1) + "</small>" + ". "
-          s << due_date_tag(version.effective_date) if version.effective_date
-          s << "<br>" +
-            progress_bar([version.closed_pourcent, version.completed_pourcent], :width => '30em', :legend => ('%0.0f%' % version.completed_pourcent))
+      def progressive_setting?(key)
+        progressive_setting(key).present?
+      end
+
+      def render_project_hierarchy_with_progress_bars(projects)
+        render_project_nested_lists(projects) do |project|
+          s = link_to_project(project, {}, :class => "#{project.css_classes} #{User.current.member_of?(project) ? 'my-project' : nil}")
+          if !progressive_setting?(:show_only_for_my_projects) || User.current.member_of?(project)
+            if progressive_setting?(:show_project_menu)
+              s << render_project_menu(project) + '<br />'.html_safe
+            end
+            if project.description.present? && progressive_setting?(:show_project_description)
+              s << content_tag('div', textilizable(project.short_description, :project => project), :class => 'wiki description')
+            end
+            if progressive_setting?(:show_project_progress) && User.current.allowed_to?(:view_issues, project)
+              s << render_project_progress_bars(project)
+            end
+          end
+          s
         end
-        s << "</div>"
       end
-      s.html_safe
-    end
 
-    def render_project_menu(project)
-      links = []
-      menu_items_for(:project_menu, project) do |node|
-        links << render_menu_node(node, project)
+      # Returns project's and its versions' progress bars
+      def render_project_progress_bars(project)
+        project.extend(Progressive::ProjectDecorator)
+        s = ''
+        if project.issues.open.any?
+          s << "<div>" + l(:label_issue_plural) + ": " +
+            link_to(l(:label_x_open_issues_abbr, :count => project.issues.open.count), :controller => 'issues', :action => 'index', :project_id => project, :set_filter => 1) +
+            " <small>(" + l(:label_total) + ": #{project.issues.count})</small> "
+          s << due_date_tag(project.opened_due_date) if project.opened_due_date
+          s << "</div>"
+          s << progress_bar([project.issues_closed_percent, project.issues_completed_percent], :width => '30em', :legend => '%0.0f%' % project.issues_closed_percent)
+        end
+
+        if project.versions.open.any?
+          s << "<div>"
+          project.versions.open.reverse_each do |version|
+            next if version.completed?
+            s << l(:label_version) + " " + link_to_version(version) + ": " +
+              link_to(l(:label_x_open_issues_abbr, :count => version.open_issues_count), :controller => 'issues', :action => 'index', :project_id => version.project, :status_id => 'o', :fixed_version_id => version, :set_filter => 1) +
+              "<small> / " + link_to_if(version.closed_issues_count > 0, l(:label_x_closed_issues_abbr, :count => version.closed_issues_count), :controller => 'issues', :action => 'index', :project_id => version.project, :status_id => 'c', :fixed_version_id => version, :set_filter => 1) + "</small>" + ". "
+            s << due_date_tag(version.effective_date) if version.effective_date
+            s << "<br>" +
+              progress_bar([version.closed_pourcent, version.completed_pourcent], :width => '30em', :legend => ('%0.0f%' % version.completed_pourcent))
+          end
+          s << "</div>"
+        end
+        s.html_safe
       end
-      links.empty? ? nil : content_tag('ul', links.join("\n").html_safe, :class => 'progressive-project-menu')
-    end
 
-    def due_date_tag(date)
-      content_tag(:time, due_date_distance_in_words(date), :class => (date < Date.today ? 'progressive-overdue' : nil), :title => date)
-    end
+      def render_project_menu(project)
+        links = []
+        menu_items_for(:project_menu, project) do |node|
+          links << render_menu_node(node, project)
+        end
+        links.empty? ? nil : content_tag('ul', links.join("\n").html_safe, :class => 'progressive-project-menu')
+      end
+
+      def due_date_tag(date)
+        content_tag(:time, due_date_distance_in_words(date), :class => (date < Date.today ? 'progressive-overdue' : nil), :title => date)
+      end
 
       alias_method_chain :render_project_hierarchy, :progress_bars
     end
