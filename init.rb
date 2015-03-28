f636a1e6fa1458202cd22926d638dbcf8d342e75
Dmitry Babenko <dmitry@ergoserv.com>
2015-03-28 16:16:38 +0200

    Version 1.1.0

--- init.rb
+++ init.rb
@@ -6,10 +6,10 @@ Redmine::Plugin.register :progressive_projects_list do
   name 'Progressive Projects List plugin'
   author 'Dmitry Babenko'
   description 'Projects List with menus and progress bars.'
-  version '1.0.0'
+  version '1.1.0'
   url 'http://stgeneral.github.io/redmine-progressive-projects-list/'
   author_url 'https://github.com/stgeneral'
-  requires_redmine :version_or_higher => '2.1.0'
+  requires_redmine :version => '2.1'..'2.6'
 
   settings :default => {
     'show_project_description'  => false,
